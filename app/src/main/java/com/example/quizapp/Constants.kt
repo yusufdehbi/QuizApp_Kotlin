b11101c738123559ed51a9431ebf171c82d82ab2
yusufdehbi <63043024+yusufdehbi@users.noreply.github.com>
2023-10-03 12:09:39 +0100

    finished

--- app/src/main/java/com/example/quizapp/Constants.kt
+++ app/src/main/java/com/example/quizapp/Constants.kt
@@ -2,6 +2,9 @@ package com.example.quizapp
 
 object Constants {
 
+    const val USER_NAME : String = "user_name"
+    const val TOTAL_QUESTIONS: String = "total_questions"
+    const val CORRECT_ANSWERS: String = "correct_answers"
     fun getQuestions(): ArrayList<Question>{
         val questionList = ArrayList<Question>()
 
